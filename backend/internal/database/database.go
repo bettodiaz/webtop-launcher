e5c219c4a1b122f560c139e30d789b9f6a3cf37e
Roberto Diaz (CachyOS) <info@bettodiaz.com>
2025-10-18 01:50:44 +0200

    go db

--- backend/internal/database/database.go
+++ backend/internal/database/database.go
@@ -81,12 +81,12 @@ func createTables() error {
 func SeedInitialData() error {
 	// Default password is 'password' - hashed with bcrypt
 	// We'll insert an admin and a normal user, plus example applications.
-	// Note: bcrypt is used in handlers when creating users; here we'll insert hashes directly for simplicity.
+	// Let the database generate the UUIDs by omitting the id column.
 	_, err := DB.Exec(`
-		INSERT INTO users (id, username, password_hash, is_admin)
+		INSERT INTO users (username, password_hash, is_admin)
 		VALUES
-			('00000000-0000-0000-0000-000000000001', 'admin', '$2a$10$7EqJtq98hPqEX7fNZaFWoOaWc0w9fQxqY0ZyZqQYq9a6/2aP8s0e', true),
-			('00000000-0000-0000-0000-000000000002', 'user', '$2a$10$7EqJtq98hPqEX7fNZaFWoOaWc0w9fQxqY0ZyZqQYq9a6/2aP8s0e', false)
+			('admin', '$2a$10$7EqJtq98hPqEX7fNZaFWoOaWc0w9fQxqY0ZyZqQYq9a6/2aP8s0e', true),
+			('user', '$2a$10$7EqJtq98hPqEX7fNZaFWoOaWc0w9fQxqY0ZyZqQYq9a6/2aP8s0e', false)
 		ON CONFLICT (username) DO NOTHING;
 		`)
 	if err != nil {
@@ -94,10 +94,10 @@ func SeedInitialData() error {
 	}
 
 	_, err = DB.Exec(`
-		INSERT INTO applications (id, name, logo_url, repository_url, docker_compose, is_enabled)
+		INSERT INTO applications (name, logo_url, repository_url, docker_compose, is_enabled)
 		VALUES
-			('app-1', 'VS Code', 'https://cdn.icon-icons.com/icons2/2107/PNG/512/file_type_vscode_icon_130084.png', 'https://github.com/linuxserver/docker-code-server', '...', true),
-			('app-2', 'Ubuntu Desktop', 'https://cdn.icon-icons.com/icons2/1508/PNG/512/ubuntu_104494.png', 'https://github.com/linuxserver/docker-webtop', '...', true)
+			('VS Code', 'https://cdn.icon-icons.com/icons2/2107/PNG/512/file_type_vscode_icon_130084.png', 'https://github.com/linuxserver/docker-code-server', '...', true),
+			('Ubuntu Desktop', 'https://cdn.icon-icons.com/icons2/1508/PNG/512/ubuntu_104494.png', 'https://github.com/linuxserver/docker-webtop', '...', true)
 		ON CONFLICT (id) DO NOTHING;
 		`)
 	return err
